49589bbc5bae10e9a96d71714736c1b2473c729d
Apoorva <144596010+Apoorva-Sinhaa@users.noreply.github.com>
2025-12-04 06:48:06 +0530

    Update placeholder for fname input field

--- crudapp/forms.py
+++ crudapp/forms.py
@@ -17,7 +17,7 @@ class OrderForm(forms.ModelForm):
 
         widgets  ={
             'oid' : forms.NumberInput(attrs={'placeholder': 'eg. 101'}),
-            'fname' : forms.TextInput(attrs={'placeholder': 'eg. Apoorvat'}),
+            'fname' : forms.TextInput(attrs={'placeholder': 'eg. Apoorva'}),
             'lname' : forms.TextInput(attrs={'placeholder': 'eg. Sinha'}),
             'price' : forms.NumberInput(attrs={'placeholder': 'eg. 1000.00'}),
             'mail' : forms.EmailInput(attrs={'placeholder': 'eg. abc@xyz.com'}),
